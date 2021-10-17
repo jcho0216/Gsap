b5fdfee4513c7d3e5fa28da2fd96ed9b4bb52f57
Jun Joy <chojuneseo1@gmail.com>
2021-10-18 08:44:10 +0900

    DELETE: unnecessary files

--- hero-gsap/src/App.js
+++ hero-gsap/src/App.js
@@ -1,77 +1,97 @@
-import React, { useEffect, useRef } from 'react';
-import {TweenMax, TimelineLite, Power3, CustomBounce } from 'gsap';
-import './App.scss';
+import React, { useEffect, useRef } from "react";
+import { TweenMax, TimelineLite, Power3 } from "gsap";
+import "./App.scss";
 //assets
-import imageGirl from './images/girl.webp';
-import imageBoy from './images/boy.webp';
-import arrow from './images/arrow.svg';
-
+import imageGirl from "./images/girl.webp";
+import imageBoy from "./images/boy.webp";
+import arrow from "./images/arrow.svg";
 
 function App() {
-    let tl = new TimelineLite({delay: .8});
+  let tl = new TimelineLite({ delay: 0.8 });
+  //img vars
+  let app = useRef(null);
+  let images = useRef(null);
+
+  //content vars
+  let content = useRef(null);
+
+  useEffect(() => {
     //img vars
-    let app = useRef(null);
-    let images = useRef(null);
+    const girlImage = images.firstElementChild;
+    const boyImage = images.lastElementChild;
 
-    //content vars
-    let content = useRef(null);
+    const headlineFirst = content.children[0].children[0];
+    const headlineSecond = headlineFirst.nextSibling;
+    const headlineThird = headlineSecond.nextSibling;
 
-    useEffect(() => {
-      //img vars
-      const girlImage = images.firstElementChild;
-      const boyImage = images.lastElementChild;
+    const contentP = content.children[1];
+    const contentButton = content.children[2];
 
-      const headlineFirst = content.children[0].children[0];
-      const headlineSecond = headlineFirst.nextSibling;
-      const headlineThird = headlineSecond.nextSibling;
+    console.log(content, content.children[1], content.children[2]);
 
-      const contentP = content.children[1]; 
-      const contentButton = content.children[2];
+    TweenMax.to(app, 0, { css: { visibility: "visible" } });
+    //images animation
+    tl.from(girlImage, 1.2, { y: 1280, ease: Power3.easeOut }, "Start")
+      .from(
+        girlImage.firstElementChild,
+        2,
+        { scale: 1.6, ease: Power3.easeOut },
+        0.2
+      )
+      .from(boyImage, 1.2, { y: 1280, ease: Power3.easeOut }, 0.2)
+      .from(
+        boyImage.firstElementChild,
+        2,
+        { scale: 1.6, ease: Power3.easeOut },
+        0.2
+      );
 
-      console.log(content, content.children[1], content.children[2]);
-
-      TweenMax.to(app, 0, {css: {visibility: 'visible'}});
-      //images animation
-      tl.from(girlImage, 1.2, {y: 1280, ease: Power3.easeOut}, 'Start')
-      .from(girlImage.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, .2)
-      .from(boyImage, 1.2, {y: 1280, ease: Power3.easeOut}, .2)
-      .from(boyImage.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, .2)
-
-      //content animation
-      tl.staggerFrom([headlineFirst.children, headlineSecond.children, headlineThird.children], 1, {
+    //content animation
+    tl.staggerFrom(
+      [headlineFirst.children, headlineSecond.children, headlineThird.children],
+      1,
+      {
         y: 44,
         ease: Power3.easeOut,
-        delay: .8
-      }, .15, 'Start')
+        delay: 0.8,
+      },
+      0.15,
+      "Start"
+    )
 
-      .from(contentP, 1, {y:20, opacity: 0, ease: Power3.easeOut, }, 1.4)
-      .from(contentButton, 1, {y:20, opacity: 0, ease: Power3.easeOut}, 1.6)
-
-    }, [tl])
-
+      .from(contentP, 1, { y: 20, opacity: 0, ease: Power3.easeOut }, 1.4)
+      .from(contentButton, 1, { y: 20, opacity: 0, ease: Power3.easeOut }, 1.6);
+  }, [tl]);
 
   return (
-    <div className="hero" ref={el => app = el}>
+    <div className="hero" ref={(el) => (app = el)}>
       <div className="container">
         <div className="hero-inner">
           <div className="hero-content">
-            <div className="hero-content-inner" ref={el => content = el}>
+            <div className="hero-content-inner" ref={(el) => (content = el)}>
               <h1>
                 <div className="hero-content-line">
-                  <div className='hero-content-line-inner'>Relieving the burden</div>
+                  <div className="hero-content-line-inner">
+                    Relieving the burden
+                  </div>
                 </div>
                 <div className="hero-content-line">
-                  <div className='hero-content-line-inner'>of disease caused</div>
+                  <div className="hero-content-line-inner">
+                    of disease caused
+                  </div>
                 </div>
                 <div className="hero-content-line">
-                  <div className='hero-content-line-inner'>by behavior.</div>
+                  <div className="hero-content-line-inner">by behavior.</div>
                 </div>
               </h1>
-              <p>Better treats serious cardiometabolic diseases to transform
+              <p>
+                Better treats serious cardiometabolic diseases to transform
                 lives and reduce healthcare utilization through the use of
-                digital therapeutics.</p>
+                digital therapeutics.
+              </p>
               <div className="button-row">
-                <button className="explore-button">explore
+                <button className="explore-button">
+                  explore
                   <div className="arrow-icon">
                     <img src={arrow} alt="arrow" />
                   </div>
@@ -80,7 +100,7 @@ function App() {
             </div>
           </div>
           <div className="hero-images">
-            <div className="hero-images-inner" ref={el => images = el}>
+            <div className="hero-images-inner" ref={(el) => (images = el)}>
               <div className="hero-image girl">
                 <img src={imageGirl} alt="girl" />
               </div>
